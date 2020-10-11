38cab2dee8a8837caae76ef94b68b67b91a354f5
eqba1 <comtech9064@gmail.com>
2020-10-11 21:05:20 +0330

    Managin course modules and their contents

--- educa/apps/courses/views.py
+++ educa/apps/courses/views.py
@@ -8,7 +8,39 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin 
 
-# QuerySet : lsit of object for some model | order / filter / read date from DataBase
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
+from .forms import ModuleFormSet
+
+##
+# The CourseModuleUpdateView view handles the formset to add, update, and
+# delete modules for a specific course.
+##
+class CourseModuleUpdateView(TemplateResponseMixin,View):
+    template_name = 'manage/formset.html'
+    course = None 
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course, data=data)
+    
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course,
+                                        id=pk,
+                                        owner=request.user)
+        return super().dispatch(request, pk)
+    
+    def get(self, reuqest, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'coures': self.course,
+                                        'formset': formset})
+
 class OwnerMixin(object):
     def get_queryset(self):
         qs = super().get_queryset()
